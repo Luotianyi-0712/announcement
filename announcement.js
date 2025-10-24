a3c9339731420c3e7a032d6affc6819749f224e2
洛洛 <111134328+Luotianyi-0712@users.noreply.github.com>
2025-10-24 18:08:11 +0800

    Create announcement.js

---
+++ announcement.js
@@ -0,0 +1,7 @@
+{
+  "Title": "ElysiaProxy 更新公告",
+  "Content": "欢迎使用 Elysia Proxy！\n\n新功能：\n- 添加了公告系统，可以远程推送重要通知\n- 优化了代理性能\n- 修复了已知问题\n\n注意事项：\n- 请确保网络连接正常\n- 如有问题请及时反馈",
+  "Version": "v1.1.0",
+  "LastUpdated": "2024-01-15T10:30:00Z",
+  "IsImportant": false
+}
